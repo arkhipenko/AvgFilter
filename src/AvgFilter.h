00a8e065a0a4c9637e844548d68d5515fcf874ff
Anatoli Arkhipenko <arkhipenko@hotmail.com>
2021-02-02 10:26:45 -0500

    v1.0.1: samples() method added

--- src/AvgFilter.h
+++ src/AvgFilter.h
@@ -10,41 +10,48 @@
 #define _AVGFILTER_H
 
 class avgFilter {
-	public:
-		avgFilter(int aSamples, long *aStorage);
-		void 	initialize();
-		long	value(long aSample);
-		
-	private:
-		long	*iReadings;
-		int		iIndex;
-		long 	iTotal;
-		int		iCount;
-		int		iSamples;
+  public:
+    avgFilter(int aSamples, long *aStorage);
+    void    initialize();
+    long    value(long aSample);
+    long    currentValue();
+    inline int samples() { return iCount; }
+    
+  private:
+    long  *iReadings;
+    int   iIndex;
+    long  iTotal;
+    int   iCount;
+    int   iSamples;
 };
 
 
 avgFilter::avgFilter(int aSamples, long *aStorage) {
-	iSamples = aSamples; 
-	iReadings = aStorage;
-	initialize();
+  iSamples = aSamples; 
+  iReadings = aStorage;
+  initialize();
 }
 
 void avgFilter::initialize() {
-	iIndex = 0;
-	iTotal = 0;
-	iCount = 0;
-	for (int i=0; i<iSamples; i++) iReadings[i]=0;
+  iIndex = 0;
+  iTotal = 0;
+  iCount = 0;
+  for (int i=0; i<iSamples; i++) iReadings[i]=0;
 }
 
-long	avgFilter::value(long aSample) {
-	iTotal -= iReadings[iIndex];
-	iReadings[iIndex] = aSample;
-	iTotal += aSample;
-	if (++iIndex >= iSamples) iIndex = 0;
-	if (++iCount > iSamples) iCount = iSamples;
-	return (iTotal/iCount);
+long  avgFilter::value(long aSample) {
+  iTotal -= iReadings[iIndex];
+  iReadings[iIndex] = aSample;
+  iTotal += aSample;
+  if (++iIndex >= iSamples) iIndex = 0;
+  if (++iCount > iSamples) iCount = iSamples;
+  return (iTotal/iCount);
 }
+
+long  avgFilter::currentValue() {
+  return ( iCount == 0 ? 0 : iTotal/iCount );
+}
+
 
 #endif  // _AVGFILTER_H
 
